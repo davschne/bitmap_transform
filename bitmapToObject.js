06b1d80cf648f95e0318017f517888472d34156d
Meng Chen <tamulemon@gmail.com>
2015-07-10 15:10:28 -0700

    export fs readfile in a function

---
+++ bitmapToObject.js
@@ -0,0 +1,29 @@
+var fs = require('fs'),
+		buffer = require('buffer');
+		
+module.exports = function (fileName, ee) {
+	fs.readFile(fileName, function(err, data) {
+		var imageSpecs = {};
+		imageSpecs.type = data.toString('ascii', 0, 2);
+		imageSpecs.size = data.readUInt32LE(2); 
+		imageSpecs.imageStart = data.readUInt32LE(10); 
+		imageSpecs.imageWidth = data.readUInt32LE(18); 
+		imageSpecs.imageHeight = data.readUInt32LE(22); 
+		imageSpecs.colorDepth = data.readUInt32LE(28); // 24 bits per pixel or 3 bytes (no alpha value?) RGB24
+		imageSpecs.numColorInPalette = data.readUInt32LE(46);
+		imageSpecs.bufferSize = data.length;
+
+		imageSpecs.headerBuffer = Array.prototype.slice.call(data, (0,53));
+
+		var pixelArrayBuffer = Array.prototype.slice.call(data, (data.readUInt32LE(10)));
+		var pixelArray = [];
+
+		for ( var i = 0; i < pixelArrayBuffer.length; i += 3 ) {
+			pixelArray.push([data.readUInt8(pixelArrayBuffer[i]), data.readUInt8(pixelArrayBuffer[i+1]), data.readUInt8(pixelArrayBuffer[i+2])]);
+		}
+
+		imageSpecs.pixel = pixelArray;
+		ee.emit('objectCreated', imageSpecs);
+	});
+};
+
