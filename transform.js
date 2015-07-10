dd73fee81f7188cbe7cf336ea4c8dfdcdab7cbe0
davschne <davschne@gmail.com>
2015-07-10 11:38:07 -0700

    Removed alpha channel in transforms

--- transform.js
+++ transform.js
@@ -1,3 +1,5 @@
+var numColorChannels = 3;
+
 function scale(value, scale_factor, max) {
   return Math.min(value * scale_factor, max || 255);
 };
@@ -7,14 +9,14 @@ exports.brightness = function(pixels, scale_factor, max) {
   for (var i = 0; i < pixels.length; i++) {
     var pixel = pixels[i];
     var newPixel = newPixels[i];
-    for (var ch = 0; ch < 4; ch++) {
+    for (var ch = 0; ch < numColorChannels; ch++) {
       newPixel[ch] = scale(pixel[ch], scale_factor, max);
     }
   }
   return newPixels;
 };
 
-exports.scaleRGBA = function(pixels, Rscale, Gscale, Bscale, Ascale, max) {
+exports.scaleRGBA = function(pixels, Rscale, Gscale, Bscale, max) {
   var newPixels;
   for (var i = 0; i < pixels.length; i++) {
     var pixel = pixels[i];
@@ -23,7 +25,6 @@ exports.scaleRGBA = function(pixels, Rscale, Gscale, Bscale, Ascale, max) {
     newPixel[0] = scale(pixel[0], Rscale, max);
     newPixel[1] = scale(pixel[1], Gscale, max);
     newPixel[2] = scale(pixel[2], Bscale, max);
-    newPixel[3] = scale(pixel[3], Ascale, max);
   }
   return newPixels;
 };
