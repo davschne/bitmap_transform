691f36d3170d14b4fc33d988bb9f61ab10281932
Meng Chen <tamulemon@gmail.com>
2015-07-10 22:13:03 -0700

    simplify cli

--- cli.js
+++ cli.js
@@ -4,8 +4,13 @@ var objectToBitmap = require('./objectToBitmap');
 var EventEmitter = require('events').EventEmitter;
 var ee = new EventEmitter();
 
+
 var parseArgs = function(args) {
   // return {src: filename.bmp, dest: filename.bmp, transform: name, transData: obj}
+  var options = {};
+	options.src = args[2];
+	options.dest = args[3];
+  return options;
 }
 
 var options = parseArgs(process.argv);
@@ -16,11 +21,11 @@ bitmapToObject(options.src, ee);
 // when function finishes loading file, it will call:
 //   ee.emit('objectCreated', imageSpecs);
 
-ee.on('objectCreated', function(err, imageObj) {
+ee.on('objectCreated', function(err, imageSpecs) {
 
   // Transform pixel array
-  imageObj.pixels = transform[options.transform](imageObj.pixels, options.transData);
+  imageSpecs.pixels = transform.scaleGrey(imageSpecs.pixels);
 
   // Write new bitmap file from object
-  objectToBitmap(options.dest, imageObj);
+  objectToBitmap(options.dest, imageSpecs);
 });
