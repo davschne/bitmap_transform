34e456a3f413607c0e721c700216766a98556238
Meng Chen <tamulemon@gmail.com>
2015-07-13 20:08:01 -0700

    update test

--- test/test.js
+++ test/test.js
@@ -1,38 +1,38 @@
-var buildImage = require('../objectToBitmap.js'),
+var writeBmpFile = require('../objectToBitmap.js'),
 		readImage = require('../bitmapToObject.js'),
 		expect = require('chai').expect,
-		dest = './testimg.bmp',
+		dest = './testimg1.bmp',
 		fs = require('fs'),
 		EE = require('events').EventEmitter,
 		ee = new EE();
 
 describe('updated test', function() {
 
-	describe('bitmapToObject', function(done) {
+	describe('bitmapToObject', function() {
 		it('will read the bmp file, convert to an object and emit an event', function() {
-			readImage('./non-palette-bitmap.bmp', ee, done);	
-			ee.on('objectCreated', function(data) {
-				console.log(data.type);
-				expect(data.type).equal('BM');
+			readImage('./test/bitmap.bmp', ee);
+			ee.on('objectCreated', function(obj){
+				expect(obj.type).equal('BM');
 //				done();
-				});
-			
+				});	
 			});
 		});
 
 	describe('objectToBitmap', function(done) {
-		it('will write a js object back to the bmp file', function() {
-				readImage('./non-palette-bitmap.bmp', ee, done);
-				ee.on('objectCreated', function(imageSpecs) {
-					buildImage(dest, imageSpecs);
-					done();
+		it('will write a js object back to the bmp file', function(done) {
+			readImage('./test/bitmap.bmp', ee);
+		
+			ee.on('objectCreated', function(obj){
+					writeBmpFile(dest, obj);
 				});
 	
-			fs.exists('./testimg.bmp', function (exist) {
-				console.log(exist);
-				expect(exist).to.be.true;
-				done();	
-			});
+			setTimeout(function() {
+				fs.exists('./testimg1.bmp', function (exist) {
+					expect(exist).to.be.true;
+					done();
+				});
+			}, 500);
+			
 		});
 	});
 });
